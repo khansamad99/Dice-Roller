50371713e1181ce98d342c9a971e607b9a402b48
Samad Khan <samadk619@gmail.com>
2021-01-05 16:12:28 +0530

    Dice Roll added

--- App.js
+++ App.js
@@ -17,13 +17,34 @@ import Dice5 from './assets/dice5.png'
 import Dice6 from './assets/dice6.png'
 
 const App = () => {
-  const [uri,setUri] = useState(Dice2)
+  const [uri,setUri] = useState(Dice1)
 
   const changeDice = () => {
-    const dice_no = Math.floor(Math.random() * 6)
-    const path = `Dice${dice_no}`
-    console.log(path)
-    setUri(path)
+    let dice_no = Math.floor(Math.random() * 6)+1;
+    switch (dice_no) {
+      case 1:
+        setUri(Dice1);
+        break;
+      case 2:
+        setUri(Dice2);
+        break;
+      case 3:
+        setUri(Dice3);
+        break;
+      case 4:
+        setUri(Dice4);
+        break;
+      case 5:
+        setUri(Dice5);
+        break;
+      case 6:
+        setUri(Dice6);
+        break;
+
+      default:
+        setUri(Dice1);
+        break;
+    }
   }
   return (
     <View style={styles.container}>
@@ -43,16 +64,18 @@ const styles = StyleSheet.create({
     justifyContent:'center',
   },
   image:{
-    height:100,
-    width:100
+    height:200,
+    width:200
   },
   text:{
     fontSize: 30,
+    marginTop:30,
     backgroundColor: "#BB2CD9",
     paddingVertical: 10,
     paddingHorizontal: 40,
     color: "#FFFFFF",
-    borderRadius: 15,
+    borderRadius: 5,
+    borderWidth:4,
     textTransform: "uppercase",
   }
 });
